1ecfb43672ce0d99998a68ff4f664dbdaabb9ee8
Lexie Jiang <LexieJiang@Lexies-MacBook-Pro.local>
2019-12-20 08:31:01 -0800

    Commit 2: PrivateRoute

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import axiosWithAuth from './utils/axiosWithAuth';
+import PrivateRoute from './components/PrivateRoute';
+import BubblePage from './components/BubblePage';
 
 import Login from "./components/Login";
 import "./styles.scss";
